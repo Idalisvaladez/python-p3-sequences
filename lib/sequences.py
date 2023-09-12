8c9662255515cf321e20810e15f95ada559776c1
Idalis Valadez <idalisvaladez22@gmail.com>
2023-09-12 13:37:16 -0500

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    numbers = []
+    for i in range(0, length):
+        if (i < 2):
+            numbers.append(i)
+        else:
+            numbers.append(numbers[-1] + numbers[-2])
+        
+    print(numbers)
